f0e8b1820d6c3f9f9c03bb73cdbdfff48140d1bb
Adrian <adrigalileo@gmail.com>
2022-04-27 22:17:24 +0200

    move logic to a class and make script to calculate the report

--- report.rb
+++ report.rb
@@ -1,55 +1,55 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'csv'
+class Report
+  require 'csv'
 
-DATA_PATH = './data.csv'
-EXPENSES = {
-  transportation: {
-    normal: 0.12,
-    discount: 0.08,
-    num_discount: 100
-  },
-  meal: {
-    normal: 10,
-    discount: 6,
-    num_discount: 3
-  },
-  parking: {
-    normal: 1,
-    discount: 0.5,
-    num_discount: 20
-  }
-}.freeze
+  DATA_PATH = 'data.csv'
+  EXPENSES = {
+    transportation: {
+      normal: 0.12,
+      discount: 0.08,
+      num_discount: 100
+    },
+    meal: {
+      normal: 10,
+      discount: 6,
+      num_discount: 3
+    },
+    parking: {
+      normal: 1,
+      discount: 0.5,
+      num_discount: 20
+    }
+  }.freeze
 
-def calculate
-  initialize_expenses
-  data = CSV.read DATA_PATH
-  data.each do |concept, units|
-    add_expense(concept, units.to_i)
+  def initialize
+    @expenses = {}
+    EXPENSES.each_key do |concept|
+      @expenses[concept] = 0
+    end
   end
-  p calculate_refund
-end
 
-def initialize_expenses
-  @expenses = {}
-  EXPENSES.each_key do |concept|
-    @expenses[concept] = 0
+  def calculate
+    data = CSV.read DATA_PATH
+    data.each do |concept, units|
+      add_expense(concept, units.to_i)
+    end
+    calculate_refund
+  end
+
+  private
+
+  def add_expense(concept, units)
+    @expenses[concept.to_sym] += units
+  end
+
+  def calculate_refund
+    total = 0
+    @expenses.each do |concept, units|
+      discount_refund = units - EXPENSES[concept][:num_discount] if units > EXPENSES[concept][:num_discount]
+      total += EXPENSES[concept][:normal] * (units - discount_refund)
+      total += EXPENSES[concept][:discount] * discount_refund
+    end
+    total
   end
 end
-
-def add_expense(concept, units)
-  @expenses[concept.to_sym] += units
-end
-
-def calculate_refund
-  total = 0
-  @expenses.each do |concept, units|
-    discount_refund = units - EXPENSES[concept][:num_discount] if units > EXPENSES[concept][:num_discount]
-    total += EXPENSES[concept][:normal] * (units - discount_refund)
-    total += EXPENSES[concept][:discount] * discount_refund
-  end
-  total
-end
-
-calculate
